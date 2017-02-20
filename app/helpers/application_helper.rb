2db29a51e9f02d1f54e66e04d44c8096821885ae
Pranav Chaturvedi <pranav.g10@gmail.com>
2017-02-20 23:18:14 +0530

    Add AppConfig helper to parse and check app_config env vars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+
+  class AppConfig
+
+    def self.json
+      JSON.parse ENV['APP_CONFIG']
+    end
+
+    def self.min_android_version
+      json['android_version'].to_i
+    end
+
+    def self.android_version_valid?(client_version)
+      client_version.to_i >= min_android_version
+    end
+
+    def self.config_version
+      json['config_version'].to_i
+    end
+
+    def self.config_version_valid?(config_version)
+      config_version.to_i == self.config_version
+    end
+
+  end
+
 end
