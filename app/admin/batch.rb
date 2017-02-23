3700af7e364cc546ed2f7368f844a8b996bc9c87
sweetymehta <smehta1289@gmail.com>
2017-02-23 11:54:31 +0530

    added download csv functionality

--- app/admin/batch.rb
+++ app/admin/batch.rb
@@ -1,49 +1,54 @@
 ActiveAdmin.register Batch do
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
 
-form title: 'Create Batch' do |f|
-	f.inputs 'Details' do
-		div id:"batch_form" do
-    # f.input :amount, collection: [200,300,400], input_html: {name: "batch[][:amount]"}, wrapper_html: {class: 'drop_down'}
-    render(:partial => 'test')
-  end
+	actions :index, :show, :create, :new
 
-end
-actions
-end	
-
-controller do
-	def create
-		Batch.generate(p_batch)
-		redirect_to :admin_batches
-	end	
-
-	private
-	def p_batch
-		amt_arr = params.require(:batch)[:amount]
-		count_arr = params.require(:batch)[:count]
-		final_arr = []
-		amt_arr.zip(count_arr).each do |x| 
-			hsh = {}
-			hsh[:amount] = x[0] 
-			hsh[:count] = x[1] 
-			final_arr << hsh
+	index do
+		id_column
+		column :location
+		column :net_worth
+		column :created_at
+		actions defaults: false do |batch|
+			link_to "Download CSV", admin_batch_path(batch)
 		end
-		final_arr
+	end
+
+	form title: 'Create Batch' do |f|
+		f.inputs 'Details' do
+			div id:"batch_form" do
+				render(:partial => 'test')
+			end
+		end
+		actions
 	end	
 
-end	
+	controller do
+		def create
+			Batch.generate(p_batch)
+			redirect_to :admin_batches
+		end	
+
+		def show
+			@batch = Batch.find(params[:id])
+			send_data @batch.to_csv, type: 'text/csv; charset=windows-1251; header=present', 
+			disposition: "attachment; filename=batch_#{DateTime.now.to_s}.csv"
+		end	
+
+		private
+
+		def p_batch
+			amt_arr = params.require(:batch)[:amount]
+			count_arr = params.require(:batch)[:count]
+			final_arr = []
+			amt_arr.zip(count_arr).each do |x| 
+				hsh = {}
+				hsh[:amount] = x[0] 
+				hsh[:count] = x[1] 
+				final_arr << hsh
+			end
+			final_arr
+		end
+
+	end	
 
 end
