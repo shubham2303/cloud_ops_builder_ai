666f5f4981ae28e647a77aa23ac0c922bbf4761c
shubham2303 <shubhamjain.2303@gmail.com>
2017-02-27 17:44:57 +0530

    minor fix

--- app/models/token.rb
+++ app/models/token.rb
@@ -4,8 +4,7 @@ class Token < ApplicationRecord
   before_save :update_other_fields
 
   def update_other_fields
-    self.token =  ShortUUID.unique
-    self.expiry =  Time.now + 1.minute
+    self.assign_attributes(expiry:  Time.now.utc + 1.hour, token: ShortUUID.unique)
   end
 
   def expired?
