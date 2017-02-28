8226bdbda4a6f32555c7b3508c9a7cbc67f91296
shubham2303 <shubhamjain.2303@gmail.com>
2017-02-28 14:29:53 +0530

    add name field to business

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -7,39 +7,39 @@ module Api
         render json: {status: 1, data: @data}
       end
 
-      # def create
-      #   type= AppConfig.type_exist?(@data['type'])
-      #   if type.nil?
-      #     render json: {status: 0, message: "type is not valid"}
-      #     return
-      #   elsif !AppConfig.subtype_exist?(type, @data['subtype'])
-      #     render json: {status: 0, message: "subtype is not valid"}
-      #     return
-      #   else
-      #   end
-      #   begin
-      #     card = Card.verify_and_use(@data['number'], @data['amount'])
-      #     batch = card.batch
-      #   rescue Exception => msg
-      #     render json: {status: 0, message: msg}
-      #     return
-      #   end
-      #   if @data['uuid'][0] == 'I'
-      #     individual = Individual.find_by!(uuid: @data['uuid'])
-      #   else
-      #     business = Business.find_by!(uuid: @data['uuid'])
-      #   end
-      #   byebug
-      #   collection = Collection.create!(type: @data['type'], subtype: @data['subtype'], number: @data[:number], amount: @data['amount'])
-      #   render json: {status: 1, data: {collection: collection}}
-      #
-      # end
-      #
-      # private
-      #
-      # def collection_params
-      #   params.require(:collection).permit(:type, :subtype, :number, :amount, :agent_id, :individual_id, :business_id)
-      # end
+      def create
+        type= AppConfig.type_exist?(@data['type'])
+        if type.nil?
+          render json: {status: 0, message: "type is not valid"}
+          return
+        elsif !AppConfig.subtype_exist?(type, @data['subtype'])
+          render json: {status: 0, message: "subtype is not valid"}
+          return
+        else
+        end
+        begin
+          card = Card.verify_and_use(@data['number'], @data['amount'])
+          # batch = card.batch
+        rescue Exception => msg
+          render json: {status: 0, message: msg}
+          return
+        end
+        if @data['uuid'][0] == 'I'
+          individual = Individual.find_by!(uuid: @data['uuid'])
+        else
+          business = Business.find_by!(uuid: @data['uuid'])
+        end
+        byebug
+        collection = Collection.create!(type: @data['type'], subtype: @data['subtype'], number: @data[:number], amount: @data['amount'])
+        render json: {status: 1, data: {collection: collection}}
+
+      end
+
+      private
+
+      def collection_params
+        params.require(:collection).permit(:type, :subtype, :number, :amount, :agent_id, :individual_id, :business_id)
+      end
     end
   end
 end
--- db/migrate/20170221113223_create_business.rb
+++ db/migrate/20170221113223_create_business.rb
@@ -1,6 +1,7 @@
 class CreateBusiness < ActiveRecord::Migration[5.0]
   def change
     create_table :businesses do |t|
+      t.string :name
       t.text :address
       t.float :turnover
       t.string :year
