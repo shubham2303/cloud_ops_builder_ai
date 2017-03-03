73bd284dcd139f420f46a344b77f9a5db02b98fd
sweetymehta <smehta1289@gmail.com>
2017-03-03 15:46:24 +0530

    base url redirection

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  get '/', to: 'admin/dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, format: false, defaults: {format: :json} do
