9fb525b49fc590c8641ef77ff3773564c558fe23
Pranav Chaturvedi <pranav.g10@gmail.com>
2017-03-03 15:04:22 +0530

    change seeds and remove default admin user from migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'pranav@appstreet.io', password: 'password', password_confirmation: 'password', role: 'super_admin')
+AdminUser.create!(email: 'mn@contecglobal.com', password: 'password', password_confirmation: 'password', role: 'super_admin')
