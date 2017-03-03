5b45f9dbfe62af36ddb8a438096d988661cd73ae
sweetymehta <smehta1289@gmail.com>
2017-03-03 12:57:38 +0530

    agent phone validation

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -3,8 +3,9 @@ class Agent < ApplicationRecord
   has_one :token
   has_many :collections
 
-  validates_inclusion_of :state, :in => JSON.parse(ENV["APP_CONFIG"])['states'], :allow_nil => true
-  validates_inclusion_of :lga, :in => JSON.parse(ENV["APP_CONFIG"])['lga'], :allow_nil => true
+  validates_inclusion_of :state, :in => JSON.parse(ENV["APP_CONFIG"])['states'], :allow_blank => true
+  validates_inclusion_of :lga, :in => JSON.parse(ENV["APP_CONFIG"])['lga'], :allow_blank => true
 
   validates_numericality_of :phone
+  validates :phone, presence: true
 end
