137e6238bd81174f5d72c90e823751e014b8b57c
shubham2303 <shubhamjain.2303@gmail.com>
2017-03-07 18:33:54 +0530

    bulk creation and some validations

--- app/admin/agent.rb
+++ app/admin/agent.rb
@@ -2,6 +2,7 @@ ActiveAdmin.register Agent do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
+actions :bulk, :index, :new, :create
 permit_params :phone, :name, :address, :birthplace, :state, :lga
 #
 # or
@@ -11,8 +12,12 @@ permit_params :phone, :name, :address, :birthplace, :state, :lga
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-
 index do
+  if params[:error_no_array]
+    div do
+      render 'error_form', { error_no_array: params[:error_no_array] }
+    end
+  end
   id_column
   column :name
   column :phone
@@ -22,9 +27,14 @@ index do
   column :lga
   column :created_at
   actions
-end 
+end
 
-form do |f|
+action_item only: :index, method: :post do
+  link_to 'Create agents', admin_agents_bulk_path
+end
+
+
+  form do |f|
   f.inputs "" do
     f.input :phone
     f.input :name
@@ -34,13 +44,38 @@ form do |f|
     f.input :lga, :label => "LGA", collection: JSON.parse(ENV["APP_CONFIG"])['lga'], prompt: 'Please select'
   end
   f.actions
-end
-
-controller do
-  def active_admin_collection
-    super.accessible_by current_ability
   end
-	# include ActiveAdminCanCan
-end	
+
+
+  controller do
+
+    def bulk
+
+    end
+
+    def bulk_creation
+      phone_arr = params[:agents][:count].reject(&:blank?)
+      phone_arr = phone_arr.uniq
+      tmp_arr =[]
+      @error_no_array = []
+      phone_arr.each do |phone|
+        agent = Agent.find_by(phone: "234#{phone}")
+        if agent.nil?
+          tmp_arr<< {phone: "234#{phone}"}
+        else
+          @error_no_array << phone
+        end
+
+      end
+
+      Agent.create!(tmp_arr)
+      redirect_to admin_agents_path("error_no_array"=> @error_no_array)
+    end
+
+    def active_admin_collection
+      super.accessible_by current_ability
+    end
+    # include ActiveAdminCanCan
+  end
 
 end
--- app/models/individual.rb
+++ app/models/individual.rb
@@ -7,6 +7,7 @@ class Individual < ApplicationRecord
 
   validates :name, :phone, presence: true
   validates_numericality_of :phone
+  validates_length_of :phone, :maximum => 11
 
   def update_uuid
     self.uuid = ShortUUID.create("I")
