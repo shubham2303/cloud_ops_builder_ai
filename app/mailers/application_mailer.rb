3998357b01be13e1b686d9c68fcacc3c6808c893
Pranav Chaturvedi <pranav.g10@gmail.com>
2017-03-10 14:21:00 +0530

    Mailer: typo fix

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,7 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
 
-  def send_stat(file, admin_user period = nil)
+  def send_stat(file, admin_user, period = nil)
     subject = "ESIR Connect Report"
     subject += " :: #{period}" unless period.nil?
   	@username = 'Admin' #current_admin_user.name
