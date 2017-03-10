e3e897940c0243102e3b65de516034ca0d148bd0
sweetymehta <smehta1289@gmail.com>
2017-03-10 16:53:25 +0530

    assets precompile

---
+++ public/assets/input_validation-50d35507a8d1ce21428c3dc9274fd621ea4a1d552a4a2a28fd85b391327ca452.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(document).on('click',".submit_valid",function(){
+        $(".phone_margin").remove();
+        // $(".inline-errors").remove();
+        var value = $(".phone_valid").val();
+        if (value.length >11 || value.length < 10) {
+            $('.phone_valid').after(
+                '<div class="c_dark_red phone_margin">length should be between 10 or 11 digits</div>'
+            );
+            return false;
+        }
+        else if((value.length == 11 && value[0] != '0')||(value.length == 10 && value[0] == '0')){
+            $('.phone_valid').after(
+                '<div class="c_dark_red phone_margin">please enter correct number</div>'
+            );
+            return false;
+        }
+
+    });
+
+    // $(".flashes" ).fadeOut(5000);
+});
