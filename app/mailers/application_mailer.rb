07755771abc9e1bcfdf79f26b3b774718649bf40
Pranav Chaturvedi <pranav.g10@gmail.com>
2017-03-10 14:02:31 +0530

    Stat mailer format change

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,10 +2,12 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
 
-  def send_stat(file, admin_user)
+  def send_stat(file, admin_user period = nil)
+    subject = "ESIR Connect Report"
+    subject += " :: #{period}" unless period.nil?
   	@username = 'Admin' #current_admin_user.name
     attachments['report.xlsx'] = file
-    mail to: admin_user.email, subject: "report"
+    mail to: admin_user.email, subject: subject
   end
 
 end
