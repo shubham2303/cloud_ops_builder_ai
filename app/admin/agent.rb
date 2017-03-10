809bf14c066dff63de12c08b341ed69fb7ea01a4
sweetymehta <smehta1289@gmail.com>
2017-03-10 15:27:25 +0530

    changed label for bulk operation on UI

--- app/admin/agent.rb
+++ app/admin/agent.rb
@@ -35,8 +35,11 @@ action_item(:index, method: :post) do
   link_to 'Bulk Agents', admin_bulk_path
 end
 
+collection_action :bulk, :title => "Create Bulk Agents" do
+end
 
-  form do |f|
+
+form do |f|
   f.inputs "" do
     phone = f.object.new_record? ? '' : f.object.phone.last(10)
     f.input :phone, :input_html => { :class => 'phone_valid', :type => "number", value: phone }
@@ -47,53 +50,53 @@ end
     f.input :state, collection: JSON.parse(ENV["APP_CONFIG"])['states'], prompt: 'Please select'
     f.input :lga, :label => "LGA", collection: JSON.parse(ENV["APP_CONFIG"])['lga'], prompt: 'Please select'
   end
-    f.actions do
-      f.action :submit, :wrapper_html => { :class => 'submit_valid'}
-      f.action :cancel, :wrapper_html => { :class => 'cancel'}
-    end
+  f.actions do
+    f.action :submit, :wrapper_html => { :class => 'submit_valid'}
+    f.action :cancel, :wrapper_html => { :class => 'cancel'}
+  end
+end
+
+
+controller do
+
+  def bulk
+    @error_csv_invalidate = nil
   end
 
-
-  controller do
-
-    def bulk
-      @error_csv_invalidate = nil
+  def bulk_creation
+    arr = []
+    phone_lga = params[:phone_lga].split(/[\r\n]+/)
+    phone_lga.each do |a|
+      arr<< a.split(", ")
     end
-
-    def bulk_creation
-      arr = []
-      phone_lga = params[:phone_lga].split(/[\r\n]+/)
-      phone_lga.each do |a|
-        arr<< a.split(", ")
+    valid_csv_array = []
+    arr.each do |a|
+      unless a.size == 2
+        flash.now[:error] = "csv invalidate"
+        render "admin/agents/bulk"
+        return
+      else
+        valid_csv_array << a
       end
-      valid_csv_array = []
-      arr.each do |a|
-        unless a.size == 2
-          flash.now[:error] = "csv invalidate"
-          render "admin/agents/bulk"
-          return
+    end
+    @error_no_array = []
+    q=""
+    valid_csv_array.each do |a|
+
+      if (a[0].length > 11) || (a[0].length == 11 && a[0][0] != '0') || (a[0].length < 10) || (a[0].length == 10 && a[0][0] == '0')
+        @error_no_array << a[0]
+      else
+        last_ten_digit_phone = a[0].last(10)
+        new_agent = Agent.new(phone: "234#{last_ten_digit_phone}", lga: a[1])
+        if new_agent.valid?
+          q +="('234#{a[0]}','#{a[1]}',now(), now()),"
         else
-          valid_csv_array << a
-        end
-      end
-      @error_no_array = []
-      q=""
-      valid_csv_array.each do |a|
-
-        if (a[0].length > 11) || (a[0].length == 11 && a[0][0] != '0') || (a[0].length < 10) || (a[0].length == 10 && a[0][0] == '0')
           @error_no_array << a[0]
-        else
-          last_ten_digit_phone = a[0].last(10)
-            new_agent = Agent.new(phone: "234#{last_ten_digit_phone}", lga: a[1])
-            if new_agent.valid?
-              q +="('234#{a[0]}','#{a[1]}',now(), now()),"
-            else
-              @error_no_array << a[0]
-            end
         end
       end
-      unless q.empty?
-        values = q.first(-1)
+    end
+    unless q.empty?
+      values = q.first(-1)
         # ActiveRecord::Base.connection.execute "INSERT INTO agents (phone, lga, created_at, updated_at) values"+values
         ActiveRecord::Base.connection.execute "INSERT INTO agents (phone, lga, created_at, updated_at) values"+values+ " ON CONFLICT DO NOTHING;"
       end
