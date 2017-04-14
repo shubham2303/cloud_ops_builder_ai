293dca74dd3b8e028ee02d3b099c92ded0cb177e
Pranav Chaturvedi <pranav.g10@gmail.com>
2017-04-14 13:03:20 +0530

    Fix migration to handle deleted agent scenario

--- db/migrate/20170412055509_add_uuid_to_collection.rb
+++ db/migrate/20170412055509_add_uuid_to_collection.rb
@@ -3,8 +3,21 @@ class AddUuidToCollection < ActiveRecord::Migration[5.0]
     add_column :collections, :uuid, :string
     add_index :collections, :uuid, :unique => true
     Collection.all.each do |b|
-      b.uuid = Collection.generate_uuid(b.agent.id, b.created_at)
+      begin
+        b.uuid = Collection.generate_uuid(b.agent_id || 0, b.created_at)
+        b.save!
+      rescue
+        try_again(b)
+      end
+    end
+  end
+
+  def try_again(b)
+    begin
+      b.uuid = 15.times.map{rand(10)}.join
       b.save!
+    rescue
+      try_again(b)
     end
   end
 end
