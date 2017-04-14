58ab84dcc2c142ebeb2309bbfbcbccc21db63684
Pranav Chaturvedi <pranav.g10@gmail.com>
2017-04-14 13:32:36 +0530

    Remove collection uuid index and value generation for now

--- db/migrate/20170412055509_add_uuid_to_collection.rb
+++ db/migrate/20170412055509_add_uuid_to_collection.rb
@@ -1,23 +1,23 @@
 class AddUuidToCollection < ActiveRecord::Migration[5.0]
   def change
     add_column :collections, :uuid, :string
-    add_index :collections, :uuid, :unique => true
-    Collection.all.each do |b|
-      begin
-        b.uuid = Collection.generate_uuid(b.agent_id || 0, b.created_at)
-        b.save!
-      rescue
-        try_again(b)
-      end
-    end
+    # add_index :collections, :uuid, :unique => true
+    # Collection.all.each do |b|
+    #   begin
+    #     b.uuid = Collection.generate_uuid(b.agent_id || 0, b.created_at)
+    #     b.save!
+    #   rescue
+    #     try_again(b)
+    #   end
+    # end
   end
 
-  def try_again(b)
-    begin
-      b.uuid = 15.times.map{rand(10)}.join
-      b.save!
-    rescue
-      try_again(b)
-    end
-  end
+  # def try_again(b)
+  #   begin
+  #     b.uuid = 15.times.map{rand(10)}.join
+  #     b.save!
+  #   rescue
+  #     try_again(b)
+  #   end
+  # end
 end
