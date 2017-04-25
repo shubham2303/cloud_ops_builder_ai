a5b75523c16beccb1991dc84e0d2e3aaf9e92b0a
Shubham Jain <shubhamjain.2303@gmail.com>
2017-04-25 20:51:09 +0530

    reports generation for daily and weekly at 23:55

--- app/workers/stat_generation_worker.rb
+++ app/workers/stat_generation_worker.rb
@@ -10,16 +10,16 @@ class StatGenerationWorker
       start_date = end_date.beginning_of_week
       Stat.to_xlsx(start_date, end_date, "weekly", -60)
     end
-    last_date = DashboardHelper.last_date_of_month? end_date
-    if last_date
-      start_date = end_date.beginning_of_month
-      Stat.to_xlsx(start_date, end_date, "monthly", -60)
-    end
-    month = end_date.strftime('%m').to_i
-    if (last_date) && (month %3 == 0)
-      start_date = end_date.beginning_of_quarter
-      Stat.to_xlsx(start_date, end_date, "quarterly", -60)
-    end
+    # last_date = DashboardHelper.last_date_of_month? end_date
+    # if last_date
+    #   start_date = end_date.beginning_of_month
+    #   Stat.to_xlsx(start_date, end_date, "monthly", -60)
+    # end
+    # month = end_date.strftime('%m').to_i
+    # if (last_date) && (month %3 == 0)
+    #   start_date = end_date.beginning_of_quarter
+    #   Stat.to_xlsx(start_date, end_date, "quarterly", -60)
+    # end
   end
 
   class << self
