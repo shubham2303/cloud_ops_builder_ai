e37307b3cc61e589c9734c5fbdbdfe5afb06388e
Pranav Chaturvedi <pranav.g10@gmail.com>
2017-05-04 10:28:44 +0530

    Optimize migrations to use less query and data loading

--- db/migrate/20170502092954_add_remaining_amount_column_to_batch_detail.rb
+++ db/migrate/20170502092954_add_remaining_amount_column_to_batch_detail.rb
@@ -2,7 +2,7 @@ class AddRemainingAmountColumnToBatchDetail < ActiveRecord::Migration[5.0]
   def change
     add_column :batch_details, :remaining_amount, :integer
     BatchDetail.all.each do |bd|
-      coll_amount = Collection.where(number: bd.n).sum(&:amount)
+      coll_amount = Collection.where(number: bd.n).sum(:amount)
       if coll_amount == 0
         bd.remaining_amount = bd.amount
       else
--- db/migrate/20170502101741_add_default_value_to_existing_column_of_agent_amount.rb
+++ db/migrate/20170502101741_add_default_value_to_existing_column_of_agent_amount.rb
@@ -1,7 +1,5 @@
 class AddDefaultValueToExistingColumnOfAgentAmount < ActiveRecord::Migration[5.0]
   def change
-    Agent.where(amount: nil).each do |a|
-      a.update!(amount: 0.0)
-    end
+    Agent.where(amount: nil).update_all(amount: 0.0)
   end
 end
