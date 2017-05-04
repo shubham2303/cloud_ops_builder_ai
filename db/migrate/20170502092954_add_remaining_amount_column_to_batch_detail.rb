35dbdba3b50e5fb1385c7c29b680d385cc9efa62
Pranav Chaturvedi <pranav.g10@gmail.com>
2017-05-04 10:59:04 +0530

    migration: Replace remaining_amount loop single update_all query

--- db/migrate/20170502092954_add_remaining_amount_column_to_batch_detail.rb
+++ db/migrate/20170502092954_add_remaining_amount_column_to_batch_detail.rb
@@ -1,14 +1,6 @@
 class AddRemainingAmountColumnToBatchDetail < ActiveRecord::Migration[5.0]
   def change
     add_column :batch_details, :remaining_amount, :integer
-    BatchDetail.all.each do |bd|
-      coll_amount = Collection.where(number: bd.n).sum(:amount)
-      if coll_amount == 0
-        bd.remaining_amount = bd.amount
-      else
-        bd.remaining_amount = bd.amount - coll_amount
-      end
-      bd.save!
-    end
+    BatchDetail.update_all('remaining_amount = batch_details.amount - (SELECT SUM(collections.amount) FROM collections WHERE collections.number = batch_details.n)')
   end
 end
